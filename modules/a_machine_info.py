922b3a6b4041650d9440053d3df3359eca2c5108
yyg27 <yyg27@outlook.com>
2025-10-24 02:39:59 +0300

    -added logging -gave polish

--- modules/a_machine_info.py
+++ modules/a_machine_info.py
@@ -8,18 +8,24 @@ logging.basicConfig(
 );
 
 def get_machine_info():
-    hostname = socket.gethostname();
-    #ip_address = socket.gethostbyname(hostname);
-    #socket.gethostbyname(hostname) just returns the loop-back address of 127.0.1.1 for my arch-linux system, not the my real ip address
-    #so the solution i found is that you need to create a UDP socket by using SOCK_DGRAM and connect it to the Google's DNS "8.8.8.8" to 
-    #trick linux OS to give it's real ip address
+
+    print("#"*54);
+    print(" "*20 + " " + "MACHINE INFO" +" " +" "*20);
+    print("#"*54+"\n");
+
+
     try:
+        hostname = socket.gethostname();
+        #ip_address = socket.gethostbyname(hostname);
+        #socket.gethostbyname(hostname) just returns the loop-back address of 127.0.1.1 for my arch-linux system, not the my real ip address
+        #so the solution i found is that you need to create a UDP socket by using SOCK_DGRAM and connect it to the Google's DNS "8.8.8.8" to 
+        #trick linux OS to give it's real ip address
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-        logging.info(f"Hostname retrieved: {hostname}");
+        logging.info(f"Hostname retrieved..");
         s.connect(("8.8.8.8", 80));
         ip_address = s.getsockname()[0];
-        logging.info(f"Primary IP retrieved: {ip_address}");
-        s.close();        
+        logging.info(f"Primary IP retrieved..");
+        s.close();
     except Exception as e :
         ip_address = "IP Address not found";
         logging.warning(f"Could not retrieve primary IP {e}");
@@ -27,29 +33,19 @@ def get_machine_info():
     ###same problem about linux. Read the bottom for info
     try:
         ip_list = socket.gethostbyname_ex(hostname)[2];
-        logging.info(f"All IP's retrieved: {ip_list}");
+        logging.info(f"All IP's retrieved..");
     except Exception as e:
         ip_list =["IP Address not found"];
         logging.warning(f"Could not retrieve all IP's: {e}");   
 
 
     ##display
-    print("\n" + "#"*50);
-    print("""
- _  _   __    ___  _  _  __  __ _  ____    __  __ _  ____  __  ____  _  _   __  ____  __  __   __ _    _  _   __  ____  _  _  __    ____ 
-( \/ ) / _\  / __)/ )( \(  )(  ( \(  __)  (  )(  ( \(  __)/  \(  _ \( \/ ) / _\(_  _)(  )/  \ (  ( \  ( \/ ) /  \(    \/ )( \(  )  (  __)                   
-/ \/ \/    \( (__ ) __ ( )( /    / ) _)    )( /    / ) _)(  O ))   // \/ \/    \ )(   )((  O )/    /  / \/ \(  O )) D () \/ (/ (_/\ ) _) 
-\_)(_/\_/\_/ \___)\_)(_/(__)\_)__)(____)  (__)\_)__)(__)  \__/(__\_)\_)(_/\_/\_/(__) (__)\__/ \_)__)  \_)(_/ \__/(____/\____/\____/(____)
-
-""");
-
-    print("#"*50);
-    print("MODULE: Machine Information Module");
+    print("\n" + "#"*54);
     print("Hostname:",hostname);
     print("Primary IP Address:",ip_address);
     ####BUGGY
     print("Other IP's",ip_list); 
-    print("#"*50 + "\n");
+    print("#"*54);
 
     #ip_list = socket.gethostbyname_ex(hostname)[2] just returns the loop-back address again like the socket.gethostbyname(hostname) for my linux system 
     #i tried to implement the UDP trick that i used for the primary IP but i could't get it to work
